b46067918fb1c64a7483c322af9e85eab944edfd
dell <1543545748@qq.com>
2025-05-17 13:22:23 +0800

    在线电影-初始

--- src/main/java/com/movies/MoviesApplication.java
+++ src/main/java/com/movies/MoviesApplication.java
@@ -1,13 +1,14 @@
 package com.movies;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MoviesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
+        log.info("Application started");
     }
-
 }
