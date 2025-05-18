1468f73813d6e72fe6e0652eb1b1ea2f433b064a
dell <1543545748@qq.com>
2025-05-18 19:56:38 +0800

    在线电影-swagger

--- src/main/java/com/movies/handler/GlobalExceptionHandler.java
+++ src/main/java/com/movies/handler/GlobalExceptionHandler.java
@@ -1,3 +1,22 @@
+package com.movies.handler;
 
 
+import com.movies.common.BaseException;
+import com.movies.common.Result;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
+@Slf4j
+@Hidden
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result handleException(BaseException e) {
+        log.error(e.getMessage());
+        return Result.error(e.getMessage());
+    }
+
+}
